cecd8b9dad1c2972efaa071d367e909794ae29d9
yonatankra <yonatan.kra@vonage.com>
2021-08-04 08:30:51 +0300

    Testing the canvas operations

--- index.spec.js
+++ index.spec.js
@@ -1,7 +1,23 @@
 const { draw } = require('./index');
 
 describe('draw', () => {
-    it(`should compile`, function () {
+    let canvas, ctx;
+    beforeEach(function() {
+        canvas = document.createElement('canvas');
+        ctx = canvas.getContext('2d');
+    });
+
+    it(`should return the shape's path`, function() {
+        const shapePath = draw(ctx);
+        expect(shapePath instanceof Path2D).toBeTruthy();
+    });
+
+    it(`should draw a house on the canvas using the main ctx`, function() {
+        draw(ctx);
+        const events = ctx.__getEvents();
+
+        expect(events).toMatchSnapshot();
+    });
 
     });
 });
