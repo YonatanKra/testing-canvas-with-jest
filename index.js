0bd70f406b52245acc8f6342f2eb558faaf67a43
yonatankra <yonatan.kra@vonage.com>
2021-08-04 08:31:21 +0300

    adding scaling and translate API

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 const castlePath = new Path2D('M 0,440 V 95 H 45.5 V 140 H 91 V 95 H 136.5 V 200 H 182 V 155 H 227.5 V 200 H 273 V 155 H 318.5 V 200 H 364 V 90 H 409.5 V 140 H 455 V 90 H 500.5 V 440 Z');
 const windowsPath = new Path2D('M 60,297 V 222 H 75.5 V 297 Z M 204.5,425 V 330 A 45.75 45.75 0 0 1 296,330 V 425 Z M 424,297 V 222 H 439.5 V 297 Z');
 
-export function draw(ctx) {
+export function draw(ctx, castleOptions = { position: { x: 0, y: 0 }, scaleX: 1, scaleY: 1 }) {
+
+    const MATRIX = new DOMMatrix();
+    const matrix = MATRIX.translate(castleOptions.position.x, castleOptions.position.y).scale(castleOptions.scaleX, castleOptions.scaleY);
 
     const shapePath = new Path2D();
     const castleShape = new Path2D();
@@ -10,9 +13,7 @@ export function draw(ctx) {
     castleShape.addPath(castlePath);
     castleWindowsShape.addPath(windowsPath);
 
-    shapePath.addPath(castleShape);
-    shapePath.addPath(castleWindowsShape);
-
+    ctx.setTransform(matrix);
     ctx.fillStyle = 'gray';
     ctx.fill(shapePath);
     ctx.fill(castleShape);
@@ -22,5 +23,6 @@ export function draw(ctx) {
     ctx.strokeStyle = 'yellow';
     ctx.stroke(shapePath);
     ctx.stroke(castleShape);
+    ctx.resetTransform();
     return shapePath;
 }
--- index.spec.js
+++ index.spec.js
@@ -1,5 +1,27 @@
 const { draw } = require('./index');
 
+(function mockDOMMatrix() {
+
+    class DOMMatrixMock extends DOMMatrix {
+        scale = jest.fn().mockImplementation((scaleX, scaleY) => this.setScale(scaleX, scaleY));
+        translate = jest.fn().mockImplementation((x, y) => this.setTranslate(x,y));
+
+        setScale(scaleX, scaleY) {
+            this.f = scaleY;
+            this.e = scaleX;
+            return this;
+        }
+
+        setTranslate(x,y){
+            this.b = x;
+            this.c = y;
+            return this;
+        }
+    }
+
+    global.DOMMatrix = DOMMatrixMock;
+})();
+
 describe('draw', () => {
     let canvas, ctx;
     beforeEach(function() {
@@ -19,5 +41,17 @@ describe('draw', () => {
         expect(events).toMatchSnapshot();
     });
 
+    it(`should draw a house on the canvas using the default scaleX and scaleY`, function() {
+        const path = draw(ctx);
+        const events = ctx.__getEvents();
+
+        expect(events).toMatchSnapshot();
+    });
+
+    it(`should draw a house with given position, scaleX and scaleY`, function() {
+        const path = draw(ctx, { position: { x: 10, y: 10 }, scaleX: .5, scaleY: 0 });
+        const events = ctx.__getEvents();
+
+        expect(events).toMatchSnapshot();
     });
 });
